4f9440243d0f4d22d12e8be466674df738212c33
eddyalbines <eddyalbines@gmail.com>
2018-06-07 18:38:57 -0400

    created controller data viaggio

--- data/datatravel.php
+++ data/datatravel.php
@@ -4,21 +4,29 @@ session_start();
 	include ('conn.inc.php');
 try{
   $dbh = new PDO($conn, $user, $pass);
-  	if (isset($_POST['FoundTravel'])) {
-     $arrivo=$_POST['arrivo'];
-      $partenza= $_POST['partenza'];
-  	$sql =$dbh->prepare("SELECT * FROM Viaggio WHERE partenza=:partenza AND arrivo=:arrivo");
+  	//if (isset($_POST['FoundTravel'])) {
+     //$arrivo=$_POST['arrivo'];
+      //$partenza= $_POST['partenza'];
+    $jsondata=array();
+    $partenza="Firenze";
+    $arrivo="Pisa";
+  	$sql =$dbh->prepare("SELECT v.*, u.idUtente, u.cognome, u.nome, u.email, u.sesso, u.nazionalita, u.telefono, u.dataNascita, u.patente FROM viaggio v inner join utente u on u.idUtente=v.idAutista WHERE v.partenza=:partenza AND v.arrivo=:arrivo ORDER BY v.data ASC LIMIT 0,30;");
   	$sql->bindValue(":arrivo", $arrivo);
       $sql->bindValue(":partenza", $partenza);
 	$sql->execute();
-  	if ($sql->rowCount()>0) {
-  	  $dati = $sql->fetchAll();
-      echo json_encode($dati);
-  	}else{
+  	 if ($sql->rowCount()>0) {
+       $jsondata['travel']=array();
+          while($row=$sql->fetch())
+          {
+  	          $jsondata['travel'][]=$row;  
+          }
+  echo json_encode($jsondata);
+         }
+  else{
   	  echo 'not_found';
   	}
   	exit();
-  }
+ // }
 
   } 
   catch (PDOException $e) {
