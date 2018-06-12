b6739ab1f4b337546e48279d336f99e7bac0e6da
eddyalbines <eddyalbines@gmail.com>
2018-06-12 14:28:03 -0400

    add

--- data/travel.php
+++ data/travel.php
@@ -3,32 +3,52 @@ session_start();
 	include ('conn.inc.php');
 try{
            $dbh = new PDO($conn, $user, $pass);
-         if (isset($_POST['offers'])) 
-         {
-                   $partenzaT= $_POST['partenzaT'];
-                  $arrivoT=$_POST['arrivoT'];
+        // if (isset($_POST['offers'])) 
+        // {
+                 //  $partenzaT= $_POST['partenzaT'];
+                  //$arrivoT=$_POST['arrivoT'];
+                  $_SESSION['user']='9';
           //$arrivoT='Pisa';
                  // $partenzaT= 'Fireasze';
                   $jsondataT=array();
-                  $sqlu =$dbh->prepare("SELECT v.*,DATE_FORMAT(v.data,  '%d/%m/%Y' ) AS dataviaggio, TIME_FORMAT( v.oraPartenza,  '%H:%i' ) AS oPartenza,TIME_FORMAT( v.oraArrivo,  '%H:%i' ) AS oArrivo,  u.idUtente, u.cognome, u.nome, u.email, u.sesso, u.nazionalita, u.telefono,u.dataNascita , u.patente FROM viaggio v inner join utente u on u.idUtente=v.idAutista WHERE v.partenza=:partenza AND v.arrivo=:arrivo ORDER BY  v.data, v.oraPartenza ASC;");
-                  $sqlu->bindValue(":arrivo", $arrivoT);
-                    $sqlu->bindValue(":partenza", $partenzaT);
+                  $jsondataT['dati'][]=array();
+                  $jsondataT['autista']=array();
+                  $jsondataT['dati']['passeggeri']=array();
+                  
+                  $sqlu =$dbh->prepare("SELECT * from viaggio where idAutista=:idAutista");
+                 // $sqlu->bindValue(":arrivo", $arrivoT);
+                  //  $sqlu->bindValue(":partenza", $partenzaT);
+                   $sqlu->bindValue(":idAutista", $_SESSION['user']); 
                      $sqlu->execute();
                     if ($sqlu->rowCount()>0)
-                    {
+                    { 
+                        $jsondataT['result']=true;
 
                         while($row=$sqlu->fetch())
-                        {
-                            $jsondataT[]=$row;  
+                        {   
+                            
+                             $sql=$dbh->prepare("Select * from utente where idUtente=:idutente limit 1");
+                            $sql->bindValue(":idutente",$_SESSION['user']);
+                            $jsondataT['dati'][]=$row;
+                            
+                            
                         }
-                          echo json_encode($jsondataT);
+                          
                        }
                    else
                    {  
                         echo json_encode("not_found");
                    }
-                    exit();
-         }
+                      $sql=$dbh->prepare("Select * from utente where idUtente=:idutente limit 1");
+                            $sql->bindValue(":idutente",$_SESSION['user']);
+                            if($sql->execute())
+                            {
+                               $autista=$sql->fetch();
+                               $jsondataT['autista'][]=$autista;
+                            }
+  echo json_encode($jsondataT);
+                    
+        // }
        if (isset($_POST['offers_past'])) 
        {
               $arrivoA=$_POST['arrivo'];
